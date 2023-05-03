736d8cb7607321709104f047785cfbe5546bca08
welu563 <ellu__1992@hotmail.com>
2023-05-03 13:15:57 +0300

    game set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { WebdevComponent } from './webdev/webdev.component';
 import { GraphicComponent } from './graphic/graphic.component';
-import { ContactComponent } from './contact/contact.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'web-development', component: WebdevComponent },
   { path: 'graphic-design', component: GraphicComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'game', component: GameComponent }
 ];
 
 @NgModule({
