2a3d54016cca2a41bfc29d2b125cda6970bf5c1c
welu563 <ellu__1992@hotmail.com>
2023-05-04 13:48:12 +0300

    css adjustments to g-carousel

--- src/app/graphic-carousel/graphic-carousel.component.ts
+++ src/app/graphic-carousel/graphic-carousel.component.ts
@@ -7,11 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphicCarouselComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  /*alempi*/
   images = [
     {
       src: 'assets/images/kulssi.jpg',
@@ -39,8 +35,58 @@ export class GraphicCarouselComponent implements OnInit {
     }
   ];
 
-  translateValue = 0;
+  /*ylempi*/
+  images2 = [
+    {
+      src: 'assets/images/kullerssoni.jpg',
+      alt: 'Image 1'
+    },
+    {
+      src: 'assets/images/kullerssoni.jpg',
+      alt: 'Image 2'
+    },
+    {
+      src: 'assets/images/kullerssoni.jpg',
+      alt: 'Image 3'
+    },
+    {
+      src: 'assets/images/kullerssoni.jpg',
+      alt: 'Image 4'
+    },
+    {
+      src: 'assets/images/kullerssoni.jpg',
+      alt: 'Image 5'
+    },
+    {
+      src: 'assets/images/kullerssoni.jpg',
+      alt: 'Image 6'
+    }
+  ];
 
+  translateValue = 0;
+  translateValue2 = 0;
+
+  intervalId: any;
+  intervalId2: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // käynnistää ajastimen, joka suorittaa nextSlide() -metodin joka 3. sekunti
+
+    /*alempi*/
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  ngOnDestroy() {
+    // lopeta ajastin
+    clearInterval(this.intervalId);
+    clearInterval(this.intervalId2);
+  }
+
+  /*alemman*/
   prevSlide() {
     if (this.translateValue === 0) {
       this.translateValue = -66.66;
@@ -49,11 +95,30 @@ export class GraphicCarouselComponent implements OnInit {
     }
   }
 
+  /*alemman*/
   nextSlide() {
     if (this.translateValue === -66.66) {
       this.translateValue = 0;
     } else {
       this.translateValue -= 33.33;
+    }
+  }
+
+  /*ylemmän*/
+  prevSlide2() {
+    if (this.translateValue2 === 0) {
+      this.translateValue2 = -66.66;
+    } else {
+      this.translateValue2 += 33.33;
+    }
+  }
+
+  /*ylemmän*/
+  nextSlide2() {
+    if (this.translateValue2 === -66.66) {
+      this.translateValue2 = 0;
+    } else {
+      this.translateValue2 -= 33.33;
     }
   }
 
