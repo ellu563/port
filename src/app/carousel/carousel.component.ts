52e2932cf3a010726395eabcb0c3886693c15715
welu563 <ellu__1992@hotmail.com>
2023-05-04 12:33:03 +0300

    making carousel component, editing mat-arrows

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -7,14 +7,34 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  public slides = [
-    { src: "assets/images/kulssi.jpg" },
-    { src: "assets/images/kulssoni.jpg" },
-    { src: "assets/images/pekkull.jpg" },
-    { src: "assets/images/kullerssoni.jpg" }
-  ];
+  slides: string[];
+  i: number;
 
-  constructor() { }
+  constructor() {
+    this.i = 0;
+    this.slides = [
+      'assets/images/1.png',
+      'assets/images/2.png',
+      'assets/images/3.png',
+      'assets/images/olut.png',
+      'assets/images/5.png',
+      'assets/images/laskutus.png',
+      'assets/images/tilaukset.png'
+    ];
+  }
+
+  getSlide() {
+    return this.slides[this.i];
+  }
+
+  getPrev() {
+    this.i == 0 ? (this.i = this.slides.length - 1) : this.i--;
+  }
+
+  getNext() {
+    this.i < this.slides.length - 1 ? this.i++ : (this.i = 0);
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/webdev/webdev.component.ts
+++ src/app/webdev/webdev.component.ts
@@ -7,34 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebdevComponent implements OnInit {
 
-  slides: string[];
-  i: number;
-
-  constructor() {
-    this.i = 0;
-    this.slides = [
-      'assets/images/1.png',
-      'assets/images/2.png',
-      'assets/images/3.png',
-      'assets/images/olut.png',
-      'assets/images/5.png',
-      'assets/images/laskutus.png',
-      'assets/images/tilaukset.png'
-    ];
-  }
-
-  getSlide() {
-    return this.slides[this.i];
-  }
-
-  getPrev() {
-    this.i == 0 ? (this.i = this.slides.length - 1) : this.i--;
-  }
-
-  getNext() {
-    this.i < this.slides.length - 1 ? this.i++ : (this.i = 0);
-  }
-
+  constructor() { }
 
   ngOnInit(): void {
   }
