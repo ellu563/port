c1dd0a1784fc16f5e42faaf33ad05fe84c4b263d
welu563 <ellu__1992@hotmail.com>
2023-05-05 14:47:08 +0300

    adding animations to all major headlines

--- src/app/graphic/graphic.component.ts
+++ src/app/graphic/graphic.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-graphic',
   templateUrl: './graphic.component.html',
   styleUrls: ['./graphic.component.css'],
+  animations: [
+    trigger('slideInLeft', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.7s ease', style({ transform: 'translateX(0%)' })),
+      ]),
+    ]),
+  ],
 
 })
 export class GraphicComponent implements OnInit {
--- src/app/webdev/webdev.component.ts
+++ src/app/webdev/webdev.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-webdev',
   templateUrl: './webdev.component.html',
-  styleUrls: ['./webdev.component.css']
+  styleUrls: ['./webdev.component.css'],
+  animations: [
+    trigger('slideInLeft', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.7s ease', style({ transform: 'translateX(0%)' })),
+      ]),
+    ]),
+  ],
 })
 export class WebdevComponent implements OnInit {
 
