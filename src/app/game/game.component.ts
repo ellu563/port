3fdb30fc06fe2286ca8372ce828d06b7983458a5
welu563 <ellu__1992@hotmail.com>
2023-05-05 13:45:07 +0300

    starting a game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Card {
+  id: number,
+  title: string;
+  frontImage: string;
+  backImage: string;
+  flipped: boolean;
+}
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
+
 export class GameComponent implements OnInit {
+
+  cards: Card[] = [
+    { id: 1, title: 'Kissa', frontImage: 'assets/images/kortti.png', backImage: 'assets/images/kullerssoni.jpg', flipped: false },
+    { id: 2, title: 'Koira', frontImage: 'assets/images/kortti.png', backImage: 'assets/images/pekkull.jpg', flipped: false },
+    { id: 3, title: 'Lehmä', frontImage: 'assets/images/kortti.png', backImage: 'assets/images/kulssi.jpg', flipped: false },
+    { id: 4, title: 'Pöllö', frontImage: 'assets/images/kortti.png', backImage: 'assets/images/olut.png', flipped: false },
+  ];
+
+  flippedCards: Card[] = [];
+  matchedCards: Card[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectCard(card: Card) {
+
+    card.flipped = !card.flipped;
+  }
 }
