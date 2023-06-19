c8c384c0180438ee2b12df7c43cebf8aa467d130
welu563 <ellu__1992@hotmail.com>
2023-06-19 14:48:13 +0300

    scss for contact me

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -5,7 +5,7 @@ import { trigger, transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css'],
+  styleUrls: ['./welcome.component.css', './welcome.component.scss'],
   animations: [
     trigger('slideInLeft', [
       transition(':enter', [
@@ -22,7 +22,39 @@ export class WelcomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /*vaihdetaan graphic-design sisalto hoveroidessa*/
+  hover: boolean = false;
 
+  /*sama web-dev osiolle*/
+  hoverWeb: boolean = false;
 
+  /*graphic design*/
+  onHover(): void {
+    this.hover = true;
+  }
+
+  /*for contact me*/
+  waveActive: boolean = false;
+
+  onLeave(): void {
+    this.hover = false;
+  }
+
+  /* web dev */
+  onHoverWeb(): void {
+    this.hoverWeb = true;
+  }
+
+  onLeaveWeb(): void {
+    this.hoverWeb = false;
+  }
+
+  /*for contact me*/
+  sendEmail() {
+    this.waveActive = true;
+    setTimeout(() => {
+      this.waveActive = false;
+    }, 1500);
+  }
 
 }
