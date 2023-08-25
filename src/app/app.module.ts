9846962533124a4515233baebd0d785732a27e3f
welu563 <ellu__1992@hotmail.com>
2023-08-25 11:06:14 +0300

    arranging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,6 @@ import { GraphicComponent } from './graphic/graphic.component';
 import { GameComponent } from './game/game.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CarouselComponent } from './carousel/carousel.component';
-import { SideGameButtonComponent } from './side-game-button/side-game-button.component';
 import { GraphicCarouselComponent } from './graphic-carousel/graphic-carousel.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -46,7 +45,6 @@ import { WelcomeImgsComponent } from './welcome-imgs/welcome-imgs.component';
     GraphicComponent,
     GameComponent,
     CarouselComponent,
-    SideGameButtonComponent,
     GraphicCarouselComponent,
     LoanComponent,
     PageNotFoundComponent,
